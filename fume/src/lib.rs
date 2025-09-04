bcbe7e9684ae42323a442ed59643af18ea21d84d
dearfl <i@flr.me>
2025-09-05 01:53:28 +0800

    unify auth with trait

--- fume/src/lib.rs
+++ fume/src/lib.rs
@@ -5,6 +5,10 @@ pub use steam::*;
 
 use fume_backend::Backend;
 
+pub trait Auth {
+    fn auth(&self) -> impl Iterator<Item = (&str, &str)>;
+}
+
 #[derive(Clone, Copy)]
 pub struct SteamApiKey {
     pub key: &'static str,
@@ -21,11 +25,23 @@ impl SteamApiKey {
     }
 }
 
+impl Auth for SteamApiKey {
+    fn auth(&self) -> impl Iterator<Item = (&str, &str)> {
+        std::iter::once(("key", self.key))
+    }
+}
+
 #[derive(Clone, Copy, Debug, Default)]
 pub struct Unauthorize;
 
 impl Unauthorize {
     pub fn with_client<B: Backend + 'static>(client: B) -> Steam<Self, B> {
         Steam::with_auth_and_client(Unauthorize, client)
+    }
+}
+
+impl Auth for Unauthorize {
+    fn auth(&self) -> impl Iterator<Item = (&str, &str)> {
+        std::iter::empty()
     }
 }
